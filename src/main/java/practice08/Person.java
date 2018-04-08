f85d8ef1dc18cfd05a6f3695527b20eaad85a070
unknown <you@example.com>
2018-04-08 16:45:54 +0800

    practice08 is running successfully!

--- src/main/java/practice08/Person.java
+++ src/main/java/practice08/Person.java
@@ -1,4 +1,44 @@
 package practice08;
 
 public class Person {
+    private int age;
+    private String name;
+    private int id;
+
+    public Person(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        Person person = (Person) o;
+        if (this.id == person.id) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return id + name.length() + age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+    public String introduce() {
+        return "My name is "+name+". I am "+age+" years old.";
+    }
 }
--- src/main/java/practice08/Student.java
+++ src/main/java/practice08/Student.java
@@ -1,4 +1,23 @@
 package practice08;
 
-public class Student {
+public class Student extends Person {
+    private Klass klass;
+    public Student(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return  klass;
+    }
+
+    public String introduce() {
+        String result = super.introduce();
+        if (klass.getLeader()!= null && klass.getLeader().getName().equals("Tom")){
+            result += " I am a Student. I am Leader of Class " + klass.getNumber() + ".";
+        } else {
+            result += " I am a Student. I am at Class " + klass.getNumber() + ".";
+        }
+        return result;
+    }
 }
