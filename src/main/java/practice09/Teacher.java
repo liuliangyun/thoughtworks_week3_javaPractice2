e27f2a56a4d5ec72a2ec6444a764e9af73aa3bc6
unknown <you@example.com>
2018-04-08 17:49:09 +0800

    practice09 is running successfully!

--- src/main/java/practice09/Teacher.java
+++ src/main/java/practice09/Teacher.java
@@ -1,4 +1,37 @@
 package practice09;
 
-public class Teacher {
+public class Teacher extends Person{
+    private Klass klass;
+    public Teacher(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public String introduce() {
+        String result = super.introduce();
+        if (klass == null){
+            result += " I am a Teacher. I teach No Class.";
+        } else {
+            result += " I am a Teacher. I teach Class " + klass.getNumber() + ".";
+        }
+        return result;
+    }
+
+    public String introduceWith(Student jerry) {
+        String result = super.introduce();
+        if (jerry.getKlass().getNumber() == klass.getNumber()) {
+            result += " I am a Teacher. I teach Jerry.";
+        } else {
+            result +=" I am a Teacher. I don't teach Jerry.";
+        }
+        return result;
+    }
 }
